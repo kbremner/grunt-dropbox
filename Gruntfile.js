7666eb032862c47dd8e0f839926a6f9aa605441c
kbremner <contact@kbremner.com>
2014-08-31 11:24:39 +0000

    moved api logic in to lib directory

--- Gruntfile.js
+++ Gruntfile.js
@@ -35,7 +35,7 @@ module.exports = function(grunt) {
           version_name: process.env.TRAVIS_BUILD_ID
         },
         files: {
-          'builds': ['dist/*']
+          'grunt-dropbox': ['dist/*']
         }
       }
     },
--- tasks/dropbox.js
+++ tasks/dropbox.js
@@ -9,8 +9,8 @@
 'use strict';
 
 module.exports = function(grunt) {
-	var request = require('request');
-  var pkg = require('package.json');
+	var dropbox = require('../lib/dropbox');
+  var dropboxClient = new dropbox();
   
   // Please see the Grunt documentation for more information regarding task
   // creation: http://gruntjs.com/creating-tasks
@@ -21,32 +21,34 @@ module.exports = function(grunt) {
     
     // Merge task-specific and/or target-specific options with these defaults.
     var options = this.options({
-      project_name: pkg.name,
-      access_token: process.env.access_token
+      access_token: process.env.dropbox_access_token
     });
     
     verifyOptions(options);
     
-    get.call(this, options, "https://api.dropbox.com/1/account/info", function(resp) {
-      grunt.log.writeln("Uploading to dropbox account for " + resp.display_name + " for project " + options.project_name + "...");
+    dropboxClient.getAccountInfo(options, function(resp) {
+      grunt.log.writeln("Uploading to dropbox account for " + resp.display_name + "...");
       
-      this.files.forEach(function(f) {
+      task.files.forEach(function(f) {
         f.src.filter(filterExists).map(function(filepath) {
           // mark that an upload has started by incrementing an in-flight count
           count += 1;
           
           // get the name of the file and construct the url for uploading it
-          var splitPath = filepath.split("/");
-          var filename = splitPath[splitPath.length - 1];
-          var dropboxPath = f.dest + "/" + options.project_name + "/" + options.version_name + "/" + filename;
-          var url = "https://api-content.dropbox.com/1/files_put/auto/" + dropboxPath;
+          var filename = getFilename(filepath);
+          var dropboxPath = f.dest + "/" + options.version_name + "/" + filename;
               
           grunt.log.writeln("uploading " + filepath + " to " + dropboxPath + "...");
           
           // read the file and start the upload, decrementing the in-flight count when complete
           // Use encoding = null to keep the file as a Buffer
-          var fileBuffer = grunt.file.read(filepath, { encoding : null });
-          put.call(task, options, url, fileBuffer, function(resp) {
+          var reqOptions = {
+            access_token: options.access_token,
+            dropboxPath: dropboxPath,
+            fileBuffer: grunt.file.read(filepath, { encoding : null })
+          }
+          
+          dropboxClient.upload(reqOptions, function(resp) {
             grunt.log.writeln(filename + " finished");
             count -= 1;
           });
@@ -69,57 +71,24 @@ module.exports = function(grunt) {
   });
   
   
+  function getFilename(filepath) {
+    var splitPath = filepath.split("/");
+    return splitPath[splitPath.length - 1];
+  }
 
   function filterExists(filepath) {
     if (!grunt.file.exists(filepath)) {
       grunt.log.warn('Source file "' + filepath + '" not found.');
       return false;
-    } else {
-      return true;
     }
+    return true;
   }
   
   function verifyOptions(options) {
-    var optionNames = ['version_name', 'project_name', 'access_token'];
+    var optionNames = ['version_name', 'access_token'];
     optionNames.forEach(function(option) {
       if(!options[option]) {
         grunt.fail.fatal(option + ' option must be specified');
-      }
-    });
-  }
-  
-  function get(options, url, callback) {
-    var reqOptions = {
-      method: "GET",
-      url: url,
-      auth: {
-        bearer: options.access_token
-      }
-    };
-    
-    req.call(this, reqOptions, callback);
-  }
-  
-  function put(options, url, body, callback) {
-    var reqOptions = {
-      method: "PUT",
-      body: body,
-      url: url,
-      auth: {
-        bearer: options.access_token
-      }
-    };
-    
-    req.call(this, reqOptions, callback);
-  }
-  
-  function req(reqOptions, callback) {
-    var task = this;
-    request(reqOptions, function(error, response, body) {
-      if(error) {
-        grunt.fail.fatal("error sending request: " + error);
-      } else {
-        callback.call(task, JSON.parse(body));
       }
     });
   }
